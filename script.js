7ad93487c30e12addbabc4ea7e8f26bf9d8a6a0f
Gabriel Gaspar Barreto <ggbarreto@id.uff.br>
2023-04-26 19:13:38 -0300

    corrigindo o requisito 3

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const verificarButtonEntrar = () => {
         if(email.value === "tryber@teste.com" && password.value === "123456"){
             alert("Olá, Tryber!");
         } else if(email.value !== "tryber@teste.com" && password.value !== "123456"){
-            alert("Email ou senha inválidos");
+            alert("Email ou senha inválidos.");
         }
     })
 };
